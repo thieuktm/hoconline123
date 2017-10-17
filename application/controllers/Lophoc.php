d1836afbf8c5a72e3000edbb7a5a717ec2c836ab
minhtuan9922 <minhtuan9922@gmail.com>
2017-10-17 23:02:17 +0700

    tuan cap nhat

--- application/controllers/Lophoc.php
+++ application/controllers/Lophoc.php
@@ -11,12 +11,21 @@ class Lophoc extends CI_Controller {
 	{
 		$data['title'] = 'CLASS ONLINE | Home';
 		$data['active'] = 1;
+		$data['content'] = 'lophoc/content';
 		$this->load->view('lophoc',$data);
 	}
 	public function cap1()
 	{
 		$data['title'] = 'CLASS ONLINE | Cấp 1';
 		$data['active'] = 2;
+		$data['content'] = 'lophoc/content';
+		$this->load->view('lophoc', $data);
+	}
+	public function baihoc()
+	{
+		$data['title'] = 'CLASS ONLINE | Bài học';
+		$data['active'] = 0;
+		$data['content'] = 'lophoc/baihoc';
 		$this->load->view('lophoc', $data);
 	}
 }
