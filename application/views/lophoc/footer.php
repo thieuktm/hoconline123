a4fddf9e7d42f275712b13b5d184cbf884b3e910
thieu <thieuktm@gmail.com>
2017-11-13 10:26:21 +0700

    cap nhat

--- application/views/lophoc/footer.php
+++ application/views/lophoc/footer.php
@@ -12,16 +12,16 @@
 					<li><a href="developers.html">Developers</a></li>
 				</ul>
 			</div>
-			<div class="footer-bottom-nav">
-				<ul>
+			<!--<div class="footer-bottom-nav">
+				<!--<ul>
 					<li><a href="terms.html">Terms</a></li>
 					<li><a href="privacy.html">Privacy</a></li>
 					<li><a href="#small-dialog4" class="play-icon popup-with-zoom-anim">Send feedback</a></li>
 					<li><a href="privacy.html">Policy & Safety </a></li>
 					<li><a href="try.html">Try something new!</a></li>
-				</ul>
-			</div>
-		</div>
+				</ul>-->
+			</div> -->
+		<!--</div>
 		<div class="footer-bottom">
 			<ul>
 				<li class="languages">
@@ -342,7 +342,7 @@
 				<li><a href="history.html" class="f-history">History</a></li>
 				<li><a href="#small-dialog5" class="play-icon popup-with-zoom-anim f-history f-help">Help</a></li>
 			</ul>
-		</div>
+		</div>-->
 	</div>
 </div>
 <!-- //footer -->
