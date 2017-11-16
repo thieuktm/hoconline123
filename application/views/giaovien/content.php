fdbd12badcb186208a37e62f9f2f2b7ff07b7343
minhtuan9922 <minhtuan9922@gmail.com>
2017-11-16 11:24:41 +0700

    cap nhat trang giaovien

--- application/views/giaovien/content.php
+++ application/views/giaovien/content.php
@@ -1,49 +1,30 @@
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-sm-3">
-			<div class="card">
-			  <img src="<?php echo base_url('images/img_avatar.png'); ?>" alt="Avatar" style="width:100%">
-			  <div class="container-card">
-				<h4><b>Nguyễn Văn A</b></h4> 
-				<p>Interior Designer</p> 
-			  </div>
-			</div>
-		</div>
-		<div class="col-sm-3">
-			<div class="card">
-			  <img src="<?php echo base_url('images/img_avatar2.png'); ?>" alt="Avatar" style="width:100%">
-			  <div class="container-card">
-				<h4><b>Jane Doe</b></h4> 
-				<p>Interior Designer</p> 
-			  </div>
-			</div>
-		</div>
-		<div class="col-sm-3">
-			<div class="card">
-			  <img src="<?php echo base_url('images/img_avatar2.png'); ?>" alt="Avatar" style="width:100%">
-			  <div class="container-card">
-				<h4><b>Jane Doe</b></h4> 
-				<p>Interior Designer</p> 
-			  </div>
-			</div>
-		</div>
-		<div class="col-sm-3">
-			<div class="card">
-			  <img src="<?php echo base_url('images/img_avatar2.png'); ?>" alt="Avatar" style="width:100%">
-			  <div class="container-card">
-				<h4><b>Jane Doe</b></h4> 
-				<p>Interior Designer</p> 
-			  </div>
-			</div>
-		</div>
-		<div class="col-sm-3">
-			<div class="card">
-			  <img src="<?php echo base_url('images/img_avatar.png'); ?>" alt="Avatar" style="width:100%">
-			  <div class="container-card">
-				<h4><b>Jane Doe</b></h4> 
-				<p>Interior Designer</p> 
-			  </div>
-			</div>
+	<div class="media">
+		<a class="pull-left" href="#">
+			<img class="media-object" src="<?=base_url('images/img_avatar.png'); ?>" alt="" width="100px">
+		</a>
+		<div class="media-body">
+			<h4 class="media-heading">Tên giáo viên</h4> 
+			Thêm nội dung
 		</div>
 	</div>
+	<div class="media">
+		<a class="pull-left" href="#">
+			<img class="media-object" src="<?=base_url('images/img_avatar.png'); ?>" alt="" width="100px">
+		</a>
+		<div class="media-body">
+			<h4 class="media-heading">Tên giáo viên</h4> 
+			Thêm nội dung
+		</div>
+	</div>
+	<div class="media">
+		<a class="pull-left" href="#">
+			<img class="media-object" src="<?=base_url('images/img_avatar.png'); ?>" alt="" width="100px">
+		</a>
+		<div class="media-body">
+			<h4 class="media-heading">Tên giáo viên</h4> 
+			Thêm nội dung
+		</div>
+	</div>
+
 </div>
